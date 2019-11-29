51ec0421e5ab5ea03c2ce732d2e483cb081ade14
Domara Shlimon <domara2000@gmail.com>
2019-11-28 22:58:52 -0500

    enemies now spawn in waves and waves have different difficulty levels

--- UnityLab5/Assets/Scripts/EnemySpawner.cs
+++ UnityLab5/Assets/Scripts/EnemySpawner.cs
@@ -22,8 +22,12 @@ public class EnemySpawner : MonoBehaviour {
 	private List<EnemyBehaviour> allEnemies;
 	private Queue<EnemyBehaviour> inactiveEnemies;
 
-	// timer
-	[SerializeField]
+	// state
+	private int enemiesLeft = 0;
+	private int waveLevel = 0;
+	private bool activeSpawning = false;
+
+	// timers
 	private float timer = 0f;
 
 	private void Awake() {
@@ -48,6 +52,11 @@ public class EnemySpawner : MonoBehaviour {
 		}
 	}
 
+	private void Start() {
+		// TODO: remove this later
+		StartWave(1);
+	}
+
 	public bool SpawnEnemy(int type) {
 		// first check if you cant spawn one
 		if (inactiveEnemies.Count == 0) return false;
@@ -69,15 +78,151 @@ public class EnemySpawner : MonoBehaviour {
 		eb.gameObject.SetActive(false);
 	}
 
-	private void Update() {
-		// TODO: remove this later
-		timer += Time.deltaTime;
-		if (timer >= SPAWN_FREQUENCY) {
-			timer -= SPAWN_FREQUENCY;
+	public void StartWave(int level) {
+		// reset timer
+		timer = 0f;
 
-			SpawnEnemy(1);
+		// set level
+		waveLevel = level;
+
+		// determin the number of enemies
+		enemiesLeft = 15 + ((level - 1) * 3);
+
+		activeSpawning = true;
+	}
+
+	private void Update() {
+		if (activeSpawning) {
+			// increase timer
+			timer += Time.deltaTime;
+			// spawn bullet
+			if (timer >= SPAWN_FREQUENCY) {
+				timer -= SPAWN_FREQUENCY;
+				// call this function to spawn the enemy
+				// it figures out the level of the enemy
+				SpawnLogic();
+			}
+
+			// end the wave if there are no more enemies to spawn
+			if (enemiesLeft == 0) {
+				Debug.Log("Wave has ended");
+				activeSpawning = false;
+				timer = 1.5f;
+			}
+		} else {
+			if (inactiveEnemies.Count == ENEMIES_TO_POOL) {
+				timer -= Time.deltaTime;
+				if (timer <= 0f) {
+					timer = 0f;
+					StartWave(waveLevel + 1);
+					Debug.Log("New wave started at level " + waveLevel);
+				}
+			}
 		}
 
+
+	}
+
+	private void SpawnLogic() {
+		// set the enemies level to this var
+		int enemylevel = 0;
+
+		#region Figuring out the level of the enemy
+
+		// roll
+		float roll = Random.Range(0f, 100f);
+
+		if (waveLevel < 6) { // levels 1 to 5
+			if (roll < 20f)
+				enemylevel = 1;
+			else
+				enemylevel = 0;
+		} else if (waveLevel < 11) { // levels 6 to 10
+			if (roll < 15f)
+				enemylevel = 2;
+			else if (roll < 54f)
+				enemylevel = 1;
+			else
+				enemylevel = 0;
+		} else if (waveLevel < 16) { // levels 11 to 15
+			if (roll < 15f)
+				enemylevel = 2;
+			else if (roll < 45f)
+				enemylevel = 1;
+			else
+				enemylevel = 0;
+		} else if (waveLevel < 21) { // levels 16 to 20
+			if (roll < 10f)
+				enemylevel = 3;
+			else if (roll < 30f)
+				enemylevel = 2;
+			else if (roll < 60f)
+				enemylevel = 1;
+			else
+				enemylevel = 0;
+		} else if (waveLevel < 26) { // levels 21 to 25
+			if (roll < 7f)
+				enemylevel = 4;
+			else if (roll < 20f)
+				enemylevel = 3;
+			else if (roll < 35f)
+				enemylevel = 2;
+			else if (roll < 65f)
+				enemylevel = 1;
+			else
+				enemylevel = 0;
+		} else if (waveLevel < 31) { // levels 26 to 30
+			if (roll < 15f)
+				enemylevel = 4;
+			else if (roll < 30f)
+				enemylevel = 3;
+			else if (roll < 55f)
+				enemylevel = 2;
+			else if (roll < 80f)
+				enemylevel = 1;
+			else
+				enemylevel = 0;
+		} else if (waveLevel < 36) { // levels 31 to 35
+			if (roll < 25f)
+				enemylevel = 4;
+			else if (roll < 45f)
+				enemylevel = 3;
+			else if (roll < 70f)
+				enemylevel = 2;
+			else if (roll < 85f)
+				enemylevel = 1;
+			else
+				enemylevel = 0;
+		} else if (waveLevel < 41) { // levels 36 to 40
+			if (roll < 30f)
+				enemylevel = 4;
+			else if (roll < 55f)
+				enemylevel = 3;
+			else if (roll < 80f)
+				enemylevel = 2;
+			else
+				enemylevel = 1;
+			// level 0 doesnt spawn anymore
+		} else {
+			if (roll < 37f)
+				enemylevel = 4;
+			else if (roll < 65f)
+				enemylevel = 3;
+			else if (roll < 80f)
+				enemylevel = 2;
+			else
+				enemylevel = 1;
+		}
+
+		#endregion
+
+		// spawn enemy
+		SpawnEnemy(enemylevel);
+
+		enemiesLeft--;
+	}
+
+	public void EndWave() {
 
 	}
 }
